07129b4c0aba5f5da82bf9e4765cbee28e49ece7
Rohitkuru <rohit.kuru@gmail.com>
2021-03-29 22:09:40 +0530

    added vpcactionclass

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import json
 import time
 
 class Cloudwatch:
+
     def __init__(self,logs,region_name):
         self.client = boto3.client(logs,region_name=region_name)
 
@@ -15,6 +16,7 @@ class Cloudwatch:
                 }
             )
             print("[INFO]: Log group created successfully")
+            #print(response['Logs']['Arn'])
         except Exception as e:
             print(e)
 
@@ -58,8 +60,8 @@ class Iam_action:
                 Description = "vpcflowrole-automation"
             )
             role_arn = response['Role']['Arn']
-
             print("[INFO]: IAM role has been created successfully")
+            return role_arn
         except Exception as e:
             print("[ERROR]: Error occured while creating IAM role {}".format(e))
 
@@ -102,13 +104,47 @@ class Iam_action:
             print("[ERROR]: Error occured while creating and attaching policy {}".format(e))
 
 
+
+class Vpc_action(Iam_action,Cloudwatch):
+
+    def __init__(self,service_name,region_name):
+        if region_name:
+            self.client = boto3.client(service_name,region_name=region_name)
+        else:
+            self.client = boto3.client(service_name)
+
+    def enable_vpc_flow_logs(self,vpc_id,role_name,loggroup):
+
+        try:
+            self.client.create_flow_logs(
+                DeliverLogsPermissionArn=role_name,
+                LogGroupName=loggroup,
+                ResourceIds=vpc_id,
+                ResourceType="VPC",
+                TrafficType="ALL",
+                LogDestinationType="cloud-watch-logs"
+            )
+            print("[INFO] VPC FLOW logs are enabled for {}".format(vpc_id))
+        except Exception as e:
+            print("[ERROR]: Error occured while enabling VPC FLOW Logs on {} ..{}".format(vpc_id,e))
+
+
+
+
+
 if __name__ == '__main__':
 
-    iam_obj = Iam_action("iam")
-    iam_obj.create_role()
+
+    iam_obj = Vpc_action("iam",region_name=None)
+    role_name = iam_obj.create_role()
     iam_obj.create_and_attach_policy()
 
-    cloudwatch_obj = Cloudwatch("logs","eu-north-1")
+    cloudwatch_obj = Vpc_action("logs","eu-north-1")
     cloudwatch_obj.create_loggroup()
     cloudwatch_obj.apply_retention_policy()
+
+    vpc_obj = Vpc_action("ec2","eu-north-1")
+    vpc_obj.enable_vpc_flow_logs(vpc_id=["vpc-0b4064180d24b0acb"],role_name=role_name,loggroup="flowlogs")
+
+
 
